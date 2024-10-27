b935093654fa714711032244d771500966168fd6
ilivinskyi <ihor.livinskyi@gmail.com>
2024-10-27 16:56:37 +0100

    add km to miles converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,12 +7,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Miles to Kilometers converter.");
         double miles = 10;
+        var km = 432;
         double kilometers = milesToKilometers(miles);
+        double milesFromKilometers = kilometerToMiles(km);
         System.out.println(miles + " miles is equal to " + kilometers + " kilometers.");
-
+        System.out.println(km + " kilometers is equal to " + milesFromKilometers + " miles.");
     }
 
     private static double milesToKilometers(double miles) {
         return miles * KM_IN_MILE;
     }
+
+    private static double kilometerToMiles(double kilometers) {
+        return kilometers / KM_IN_MILE;
+    }
 }
+
